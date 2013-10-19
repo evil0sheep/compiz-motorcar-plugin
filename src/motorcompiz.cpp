815b6b5be918b320b2f59968e24f285205fc6acf
Forrest Reiling <evil0sheep@gmail.com>
2013-10-18 23:16:11 -0700

    builds now, options show in compiz. enabling plugin crashes compiz

--- src/motorcompiz.cpp
+++ src/motorcompiz.cpp
@@ -1,9 +1,10 @@
-MyPluginScreen::MyPluginScreen (CompScreen *s):
-    PluginClassHandler <MyPluginScreen, CompScreen> (s),
+#include "motorcompiz.h"
+/*MotorScreen::MotorScreen (CompScreen *s):
+    PluginClassHandler <MotorScreen, CompScreen> (s),
     cScreen (CompositeScreen::get (screen)),
     gScreen (GLScreen::get (screen))
 {
     ScreenInterface::setHandler (screen);
     CompositeScreenInterface::setHandler (cScreen);
     GLScreenInterface::setHandler (gScreen);
-}
+}*/
--- src/motorcompiz.h
+++ src/motorcompiz.h
@@ -1,17 +1,33 @@
 #include <core/core.h>
-class MyPluginScreen :
-	public PluginClassHandler <MyScreen, CompScreen>,
-	public ScreenInterface,
-	public MypluginOptions
+#include <core/pluginclasshandler.h>
+#include <composite/composite.h>
+#include <opengl/opengl.h>
+
+class MotorScreen :
+	public PluginClassHandler <MotorScreen, CompScreen>,
+	public GLScreenInterface,
+	public CompositeScreenInterface
+{
+	public:
+		CompositeScreen *cScreen;
+		GLScreen	*gScreen;
+
+		MotorScreen (CompScreen *);
+		~MotorScreen ();
+};
+
+class MotorWindow :
+	public PluginClassHandler <MotorWindow, CompWindow>,
+	public GLWindowInterface
 {
 	public:
 
-		MyPluginScreen (CompScreen *);
-		~MyPluginScreen ();
-}
+		MotorWindow (CompWindow *);
+		~MotorWindow ();
+};
 class MyPluginVTable :
-	public CompPlugin::VTableForScreenAndWindow <MyPluginScreen, MyPluginWindow>
+	public CompPlugin::VTableForScreenAndWindow <MotorScreen, MotorWindow>
 {
 	public:
 		bool init ();
-}
+};
